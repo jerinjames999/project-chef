15621f06fb1f7dbab4acf407624d3b631424e608
jerinjames999 <jerinjameskarippa999@gmail.com>
2020-09-17 02:04:16 +0530

    Bundling and sample testing

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname,'assets/js'),
+        filename:'bundle.js'
+    }
+};
