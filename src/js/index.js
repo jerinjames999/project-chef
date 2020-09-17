df8bc87508f7101b539d52e41afb8971cfdaa340
jerinjames999 <jerinjameskarippa999@gmail.com>
2020-09-17 15:24:16 +0530

    plugins added and webpack automated

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 // Global app controller
 import num from './test'
-console.log(`I imported ${num} from other module`);
+console.log(`I imported ${num} from other module called test.js`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname,'assets/js'),
-        filename:'bundle.js'
-    }
+        path: path.resolve(__dirname,'assets'),
+        filename:'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './assets'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'./src/index.html'
+        })
+    ]
+
 };
